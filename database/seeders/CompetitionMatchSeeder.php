978a603ccbdc7201856a2ebe4e0df00f7bd7fb38
Ahmed <ahmedmohalghurbani@gmail.com>
2025-04-01 10:58:12 +0200

    everything for matches till noe

--- database/seeders/CompetitionMatchSeeder.php
+++ database/seeders/CompetitionMatchSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CompetitionMatch;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class CompetitionMatchSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CompetitionMatch::insert([
+            [
+                'competition_id' => 1,
+                'name' => 'Match 1',
+                'captain_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'competition_id' => 2,
+                'name' => 'Match 2',
+                'captain_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
