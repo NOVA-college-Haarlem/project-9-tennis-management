1f1850d69471b15229ce0e24b77b0da78caf3eaa
Ahmed <ahmedmohalghurbani@gmail.com>
2025-04-01 09:57:02 +0200

    fixed merge

--- app/Models/CompetitionMatch.php
+++ app/Models/CompetitionMatch.php
@@ -2,15 +2,11 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-=======
->>>>>>> 24878b24bd768d6b3e57e8b24e06be2faff70d42
 use Illuminate\Database\Eloquent\Model;
 
 class CompetitionMatch extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
 
     protected $fillable = ['competition_id', 'court_id', 'team1_id', 'team2_id', 'scheduled_time', 'score', 'status'];
@@ -26,10 +22,4 @@ class CompetitionMatch extends Model
     public function team2() {
         return $this->belongsTo(CompetitionTeam::class, 'team2_id');
     }
-=======
-    public function competition()
-    {
-        return $this->belongsTo(Competition::class);
-    }
->>>>>>> 24878b24bd768d6b3e57e8b24e06be2faff70d42
 }
