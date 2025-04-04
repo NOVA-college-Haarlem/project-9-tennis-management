283381393ca229e018ee53f765e7c5a41775a0cf
Ferdy <193423@novacollege.nl>
2025-04-04 10:32:29 +0200

    <x-base-layout> toegevoegd aan alle competition_match pagina's

--- resources/views/competition_matches/create.blade.php
+++ resources/views/competition_matches/create.blade.php
@@ -1,35 +1,38 @@
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif <h1>Create Competition Match</h1>
+<x-base-layout>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <h1>Create Competition Match</h1>
     <form action="{{ route('competition_matches.store') }}" method="POST">
         @csrf
         <label>Competition:</label>
         <select name="competition_id">
-            @foreach($competitions as $competition)
+            @foreach ($competitions as $competition)
                 <option value="{{ $competition->id }}">{{ $competition->name }}</option>
             @endforeach
         </select>
         <label>Team 1:</label>
         <select name="team1_id">
-            @foreach($teams as $team)
+            @foreach ($teams as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select>
         <label>Team 2:</label>
         <select name="team2_id">
-            @foreach($teams as $team)
+            @foreach ($teams as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select>
         <label>Court:</label>
         <select name="court_id">
-            @foreach($courts as $court)
+            @foreach ($courts as $court)
                 <option value="{{ $court->id }}">{{ $court->name }}</option>
             @endforeach
         </select>
@@ -40,3 +43,4 @@
         <input type="datetime-local" name="scheduled_time" required>
         <button type="submit">Create</button>
     </form>
+</x-base-layout>
