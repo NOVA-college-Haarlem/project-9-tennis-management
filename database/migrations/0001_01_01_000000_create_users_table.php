30115b1951671cb111728b7f287e39846f55cd4e
Pieter van Dijk <p.u.e.van.dijk@outlook.com>
2025-04-04 09:44:20 +0200

    roles added as comment

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->string('housenumber');
             $table->string('postal_code');
             $table->string('city');
+            // $table->enum('role', ['user','clublid', 'coach', 'competitie_coördinator', 'toernooidirecteur', 'onderhoudsmanager', 'proshopmanager', 'jeugdprogrammacoördinator', 'clubbeheerder', 'evenementencoördinator', 'gast' ])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
