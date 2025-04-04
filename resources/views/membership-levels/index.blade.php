dffff124993fabe492d545b831e07fcf19691980
Ferdy <193423@novacollege.nl>
2025-04-04 13:12:45 +0200

    <x-base-layout> toegevoegd aan membershipLevels pagina's

--- resources/views/membership-levels/index.blade.php
+++ resources/views/membership-levels/index.blade.php
@@ -1,16 +1,18 @@
-<h1>Membership Levels</h1>
-<a href="{{ route('membership-levels.create') }}">Create New Membership Level</a>
-<ul>
-    @foreach ($membershipLevels as $level)
-        <li>
-            {{ $level->name }} - ${{ $level->price }}
-            <a href="{{ route('membership-levels.show', $level->id) }}">View</a>
-            <a href="{{ route('membership-levels.edit', $level->id) }}">Edit</a>
-            <form action="{{ route('membership-levels.destroy', $level->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+<x-base-layout>
+    <h1>Membership Levels</h1>
+    <a href="{{ route('membership-levels.create') }}">Create New Membership Level</a>
+    <ul>
+        @foreach ($membershipLevels as $level)
+            <li>
+                {{ $level->name }} - ${{ $level->price }}
+                <a href="{{ route('membership-levels.show', $level->id) }}">View</a>
+                <a href="{{ route('membership-levels.edit', $level->id) }}">Edit</a>
+                <form action="{{ route('membership-levels.destroy', $level->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+</x-base-layout>
--- resources/views/membership-levels/show.blade.php
+++ resources/views/membership-levels/show.blade.php
@@ -1,15 +1,19 @@
-<h1>Membership Level Details</h1>
-<p>Name: {{ $membershipLevel->name }}</p>
-<p>Description: {{ $membershipLevel->description }}</p>
-<p>Duration: {{$membershipLevel->duration}} {{$membershipLevel->duration > 1 ? $membershipLevel->duration_unit . 's' : $membershipLevel->duration_unit}}</p>
-<p>Price: €{{ $membershipLevel->price }}</p>
-<p>Max Booking Days Ahead: {{ $membershipLevel->max_booking_days_ahead }}</p>
-<p>Max Booking Hours: {{ $membershipLevel->max_booking_hours }}</p>
-<p>Allow Guests: {{ $membershipLevel->allow_guests ? 'Yes' : 'No' }}</p>
-<p>Access Competitions: {{ $membershipLevel->access_competitions ? 'Yes' : 'No' }}</p>
-<a href="{{ route('membership-levels.edit', $membershipLevel->id) }}">Edit</a>
-<form action="{{ route('membership-levels.destroy', $membershipLevel->id) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-</form>
+<x-base-layout>
+    <h1>Membership Level Details</h1>
+    <p>Name: {{ $membershipLevel->name }}</p>
+    <p>Description: {{ $membershipLevel->description }}</p>
+    <p>Duration: {{ $membershipLevel->duration }}
+        {{ $membershipLevel->duration > 1 ? $membershipLevel->duration_unit . 's' : $membershipLevel->duration_unit }}
+    </p>
+    <p>Price: €{{ $membershipLevel->price }}</p>
+    <p>Max Booking Days Ahead: {{ $membershipLevel->max_booking_days_ahead }}</p>
+    <p>Max Booking Hours: {{ $membershipLevel->max_booking_hours }}</p>
+    <p>Allow Guests: {{ $membershipLevel->allow_guests ? 'Yes' : 'No' }}</p>
+    <p>Access Competitions: {{ $membershipLevel->access_competitions ? 'Yes' : 'No' }}</p>
+    <a href="{{ route('membership-levels.edit', $membershipLevel->id) }}">Edit</a>
+    <form action="{{ route('membership-levels.destroy', $membershipLevel->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
+</x-base-layout>
