7f31f2015160fc46fdac71c114e521c0ca5ad005
Ferdy <193423@novacollege.nl>
2025-04-04 13:20:35 +0200

    <x-base-layout> toegevoegd aan memberships pagina's

--- resources/views/memberships/create.blade.php
+++ resources/views/memberships/create.blade.php
@@ -1,25 +1,27 @@
-<h1>Create Membership</h1>
-<form action="{{ route('memberships.store') }}" method="POST">
-    @csrf
-    <label for="user_id">User:</label>
-    <select name="user_id" id="user_id">
-        @foreach ($users as $user)
-            <option value="{{ $user->id }}">{{ $user->firstname }} {{ $user->lastname }}</option>
-        @endforeach
-    </select>
-    <label for="membership_level_id">Membership Level:</label>
-    <select name="membership_level_id" id="membership_level_id">
-        @foreach ($membershipLevels as $level)
-            <option value="{{ $level->id }}">{{ $level->name }}</option>
-        @endforeach
-    </select>
-    <label for="start_date">Start Date:</label>
-    <input type="date" name="start_date" id="start_date">
-    <label for="end_date">End Date:</label>
-    <input type="date" name="end_date" id="end_date">
-    <label for="active">Active:</label>
-    <input type="checkbox" name="active" id="active" value="1">
-    <label for="balance">Balance:</label>
-    <input type="number" name="balance" id="balance" step="0.01">
-    <button type="submit">Create</button>
-</form>
+<x-base-layout>
+    <h1>Create Membership</h1>
+    <form action="{{ route('memberships.store') }}" method="POST">
+        @csrf
+        <label for="user_id">User:</label>
+        <select name="user_id" id="user_id">
+            @foreach ($users as $user)
+                <option value="{{ $user->id }}">{{ $user->firstname }} {{ $user->lastname }}</option>
+            @endforeach
+        </select>
+        <label for="membership_level_id">Membership Level:</label>
+        <select name="membership_level_id" id="membership_level_id">
+            @foreach ($membershipLevels as $level)
+                <option value="{{ $level->id }}">{{ $level->name }}</option>
+            @endforeach
+        </select>
+        <label for="start_date">Start Date:</label>
+        <input type="date" name="start_date" id="start_date">
+        <label for="end_date">End Date:</label>
+        <input type="date" name="end_date" id="end_date">
+        <label for="active">Active:</label>
+        <input type="checkbox" name="active" id="active" value="1">
+        <label for="balance">Balance:</label>
+        <input type="number" name="balance" id="balance" step="0.01">
+        <button type="submit">Create</button>
+    </form>
+</x-base-layout>
--- resources/views/memberships/index.blade.php
+++ resources/views/memberships/index.blade.php
@@ -1,17 +1,19 @@
-<h1>Memberships</h1>
-<a href="{{ route('memberships.create') }}">Create New Membership</a>
-<ul>
-    @foreach ($memberships as $membership)
-        <li>
-            {{ $membership->user->firstname }} {{ $membership->user->lastname }} -
-            {{ $membership->membershipLevel->name }}
-            <a href="{{ route('memberships.show', $membership->id) }}">View</a>
-            <a href="{{ route('memberships.edit', $membership->id) }}">Edit</a>
-            <form action="{{ route('memberships.destroy', $membership->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+<x-base-layout>
+    <h1>Memberships</h1>
+    <a href="{{ route('memberships.create') }}">Create New Membership</a>
+    <ul>
+        @foreach ($memberships as $membership)
+            <li>
+                {{ $membership->user->firstname }} {{ $membership->user->lastname }} -
+                {{ $membership->membershipLevel->name }}
+                <a href="{{ route('memberships.show', $membership->id) }}">View</a>
+                <a href="{{ route('memberships.edit', $membership->id) }}">Edit</a>
+                <form action="{{ route('memberships.destroy', $membership->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+</x-base-layout>
