6e44a94a4fa877b51e85f5b95c3f358376b2ecce
Pieter van Dijk <p.u.e.van.dijk@outlook.com>
2025-04-04 09:45:57 +0200

    lessonbooking model

--- app/Models/LessonBooking.php
+++ app/Models/LessonBooking.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LessonBooking extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'lesson_id',
+        'coach_id',
+        'status',
+        'start_time',
+        'end_time',
+    ];
+
     public function coach()
     {
         return $this->belongsTo(Coach::class);
@@ -15,5 +24,10 @@ class LessonBooking extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function court()
+    {
+        return $this->belongsTo(Court::class);
     }
 }
